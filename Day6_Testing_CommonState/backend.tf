cbe8e2c29977fdcad2dd7a5283e9185911cadba4
lokesh <lokeshyajjavarapu17@gmail.com>
2026-03-13 17:47:26 +0530

    Day6

--- Day6_Testing_CommonState/backend.tf
+++ Day6_Testing_CommonState/backend.tf
@@ -7,4 +7,7 @@ terraform {
     use_lockfile = true
   }
 }
+#here we are using the same bucket but different key for different days so that we can have separate state files for each day.
+#we can also have a common state file for all the days if we want to use the same state file for all the days then we can use the same key for all the days.
+#if we use common s3 path for two diff directories you may destory or modify existing resources.
 
