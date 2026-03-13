b348931ddc12873568b346205d158f434776405e
lokesh <lokeshyajjavarapu17@gmail.com>
2026-03-13 15:07:33 +0530

    Day6

--- Day6_Statelocking/main.tf
+++ Day6_Statelocking/main.tf
@@ -12,4 +12,5 @@ resource "aws_subnet" "name" {
     Name="Prod Subnet"
   }
   }
-  
+
+   
---
+++ Day6_Testing_CommonState/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "backendremotes3bucket"
+    #key    = "terraform.tfstate" #if same pth already using in diff directory not a good practice to use here
+    key     = "day6/terraform.tfstate" #good practice to use here
+    region = "ap-south-1"
+  }
+}
+
