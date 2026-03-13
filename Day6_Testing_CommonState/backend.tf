287c6e4a7d4b4eab0178493c77a0d3585bdf69f2
lokesh <lokeshyajjavarapu17@gmail.com>
2026-03-13 15:48:49 +0530

    day6

--- Day6_Testing_CommonState/backend.tf
+++ Day6_Testing_CommonState/backend.tf
@@ -4,6 +4,7 @@ terraform {
     #key    = "terraform.tfstate" #if same pth already using in diff directory not a good practice to use here
     key     = "day6/terraform.tfstate" #good practice to use here
     region = "ap-south-1"
+    use_lockfile = true
   }
 }
 
